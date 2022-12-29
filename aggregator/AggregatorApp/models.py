5940458802e856b425fe93109a4539f881baee56
ngrambev <neilb743@hotmail.com>
2022-12-28 22:10:39 -0800

    Changed all files.

--- aggregator/AggregatorApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# This represents the basic building block of the page.
-class Headline(models.Model):
-  title = models.CharField(max_length=200)
-  image = models.URLField(null=True, blank=True)
-  url = models.TextField()
-  def __str__(self):
-    return self.title
--- aggregator/AggregatorApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from AggregatorApp.views import scrape, home
-
-urlpatterns = [
-  path('scrape/', scrape, name="scrape"),
-  path('', home, name="home"),
-]
