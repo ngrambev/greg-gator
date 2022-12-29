ad0ccbaaf1c50ee4c5c113c9e28cecc33a887896
ngrambev <neilb743@hotmail.com>
2022-12-28 22:10:39 -0800

    Added a favicon

--- aggregator/Aggregator/urls.py
+++ aggregator/Aggregator/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", include("AggregatorApp.urls")),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path("favicon.ico",  RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")))
 ]
