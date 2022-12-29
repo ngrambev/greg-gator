2d87b0a65e86fdcef3061e9e7c5f2fac312474ef
ngrambev <neilb743@hotmail.com>
2022-12-28 22:10:39 -0800

    The sound of music.../

--- AggregatorApp/urls.py
+++ AggregatorApp/urls.py
@@ -4,13 +4,9 @@ from django.conf.urls.static import static
 # =============================================
 from django.contrib import admin
 from django.urls import path, include
+from news import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('notes/', include("notepad.urls")),
-    path('', include("news.urls")),
+    path('', views.HeadlinesView.as_view(), name='home')
 ]
-
-if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/views.py
+++ news/views.py
@@ -9,66 +9,65 @@ from django.views import View
 requests.packages.urllib3.disable_warnings()
 
 
-class Headlines(View):
-    def get(self, request):
-        return HttpResponse('result')
+class HeadlinesView(View):
+    template_name = 'views/home.html'
 
+    def get(self, request, *args, **kwargs):
+        scrape()
+        headlines = Headline.objects.all()[::-1]
+        context = {
+            'object_list': headlines,
+        }
+        return render(request, "home.html", context)
 
-def news_list(request):
-	scrape(request)
-	headlines = Headline.objects.all()[::-1]
-	context = {
-		'object_list': headlines,
-	}
-	return render(request, "home.html", context)
+def scrape():
+    # Delete all previous headlines, they may be outdated.
+    Headline.objects.all().delete()
 
-def scrape(request):
-	# Delete all previous headlines, they may be outdated.
-	Headline.objects.all().delete()
+    # Fill the database up again.
 
-	# Fill the database up again.
+    # Motortrend
+    motorTrend = "https://www.motortrend.com/"
 
-	# Motortrend
-	motorTrend = "https://www.motortrend.com/"
+    session = requests.Session()
+    content = session.get(motorTrend + "auto-news/", verify=False).content
+    soup = BSoup(content, "html.parser")
 
-	session = requests.Session()
-	content = session.get(motorTrend + "auto-news/", verify=False).content
-	soup = BSoup(content, "html.parser")
+    # Find all instances of the proper class which represents a headline.
+    News = soup.find_all('a', class_="_22VtJ")
+    for article in News:
+        # Parse out the data out of the html.
+        title = article.getText()
+        link = urljoin(motorTrend, article['href'])
 
-	# Find all instances of the proper class which represents a headline.
-	News = soup.find_all('a', class_="_22VtJ")
-	for article in News:
-		# Parse out the data out of the html.
-		title = article.getText()
-		link = urljoin(motorTrend, article['href'])
+        # Create a Headline object, and store pertinent info.
+        new_headline = Headline()
+        new_headline.title = title
+        new_headline.url = link
 
-		# Create a Headline object, and store pertinent info.
-		new_headline = Headline()
-		new_headline.title = title
-		new_headline.url = link
+        # Save the headline into the database.
+        new_headline.save()
 
-		# Save the headline into the database.
-		new_headline.save()
+    # The Drive
+    theDrive = "https://www.thedrive.com/"
 
-	# The Drive
-	theDrive = "https://www.thedrive.com/"
+    content = session.get(theDrive + "the-war-zone/", verify=False).content
+    soup = BSoup(content, "html.parser")
 
-	content = session.get(theDrive + "the-war-zone/", verify=False).content
-	soup = BSoup(content, "html.parser")
+    # Find all instances of the proper class which represents a headline.
+    News = soup.find_all('a', class_="MuiBox-root css-3f60fj")
+    for article in News:
+        # Parse out the data out of the html.
+        title = article.find(
+            'h3', class_="MuiTypography-root MuiTypography-h5 css-z31x94").getText()
+        link = urljoin(theDrive, article['href'])
 
-	# Find all instances of the proper class which represents a headline.
-	News = soup.find_all('a', class_="MuiBox-root css-3f60fj")
-	for article in News:
-		# Parse out the data out of the html.
-		title = article.find('h3', class_="MuiTypography-root MuiTypography-h5 css-z31x94").getText()
-		link = urljoin(theDrive, article['href'])
+        # Create a Headline object, and store pertinent info.
+        new_headline = Headline()
+        new_headline.title = title
+        new_headline.url = link
 
-		# Create a Headline object, and store pertinent info.
-		new_headline = Headline()
-		new_headline.title = title
-		new_headline.url = link
+        # Save the headline into the database.
+        new_headline.save()
 
-		# Save the headline into the database.
-		new_headline.save()
-
-	return redirect("../")
+    return redirect("../")
