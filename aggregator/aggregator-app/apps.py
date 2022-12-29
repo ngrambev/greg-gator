bdc371ee6ad18aaf3e457a9bc981793b368d38bd
ngrambev <neilb743@hotmail.com>
2022-12-28 22:10:38 -0800

    Renamed the application.

--- aggregator/aggregator-app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangohelloworldappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'djangohelloworldapp'
--- aggregator/aggregator-app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from djangohelloworldapp import views
-urlpatterns=[
-    path("", views.home,name="home"),
-    path('admin/',admin.site.urls),
-]
